f12d29203a3f6d915e9fd5eeaa510c6c29e94484
SanseDev <sansedev@gmail.com>
2024-10-17 19:50:08 +0200

    Design sys ..

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,9 @@ import { GeistSans } from "geist/font/sans";
 import { ThemeProvider } from "next-themes";
 import Link from "next/link";
 import "./globals.css";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { MdAccountCircle } from "react-icons/md";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -37,12 +40,16 @@ export default function RootLayout({
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
-                    <Link href={"/"}>Next.js Supabase Starter</Link>
-                    <div className="flex items-center gap-2">
-                      <DeployButton />
-                    </div>
+                    <Image 
+                    alt="Illustration du logo Skall Promo"
+                    width={50}
+                    height={50}
+                    src="/assets/image/skp-logo.png"
+                    className="rounded-2xl"/>
                   </div>
-                  {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
+                  <Button>
+                    <MdAccountCircle />
+                  </Button>
                 </div>
               </nav>
               <div className="flex flex-col gap-20 max-w-5xl p-5">
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,13 +10,163 @@ const config = {
   ],
   prefix: "",
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
+    colors: {
+
+      white: "#ffffff",
+
+      blue: {
+        100: "#dbeafe",
+        200: "#bfdbfe",
+        300: "#93c5fd",
+        400: "#60a5fa",
+        500: "#3b82f6",
+        600: "#2563eb",
+        DEFAULT: "#1d4ed8",
+        800: "#1e40af",
       },
+
+      gray: {
+        200: "#e5e7eb",
+        DEFAULT: "#d1d5db",
+        400: "#9ca3af",
+        500: "#6b7280",
+        600: "#4b5563",
+        700: "#374151",
+        800: "#1f2937",
+        900: "#111827",
+      },
+
+      alert: {
+        danger: "#FF4E4E",
+        success: "#00DD16",
+        warning: "#FEB72F",
+      },
+
     },
+
+    fontSize: {
+
+      '8xl': ['120px', {
+
+        lineHeight: '120px',
+        letterSpacing: '-6px',
+        fontWeight: '500',
+
+      }],
+
+      '7xl': ['72px', {
+
+        lineHeight: '80px',
+        letterSpacing: '-4.5px',
+        fontWeight: '600',
+
+      }],
+
+      '6xl': ['55px', {
+
+        lineHeight: '60px',
+        letterSpacing: '-2.5px',
+        fontWeight: '500',
+
+      }],
+
+      '5xl': ['48px', {
+
+        lineHeight: '54px',
+        letterSpacing: '-1.600000023841858px',
+        fontWeight: '500',
+
+      }],
+
+      '4xl': ['36px', {
+
+        lineHeight: '44px',
+        letterSpacing: '-1.2000000476837159px',
+        fontWeight: '500',
+
+      }],
+
+      '3xl': ['28px', {
+
+        lineHeight: '34px',
+        letterSpacing: '-0.800000011920929px',
+        fontWeight: '500',
+
+      }],
+
+      '2xl': ['24px', {
+
+        lineHeight: '30px',
+        letterSpacing: '-1px',
+        fontWeight: '400',
+
+      }],
+
+      xl: ['24px', {
+
+        lineHeight: '30px',
+        letterSpacing: '-1px',
+        fontWeight: '400',
+
+      }],
+
+      lg: ['21px', {
+
+        lineHeight: '30px',
+        letterSpacing: '-0.800000011920929px',
+        fontWeight: '400',
+
+      }],
+
+      base: ['17px', {
+
+        lineHeight: '25px',
+        letterSpacing: '-0.699999988079071px',
+        fontWeight: '400',
+
+      }],
+
+      sm: ['15px', {
+
+        lineHeight: '23px',
+        letterSpacing: '-0.6000000238418579px',
+        fontWeight: '400',
+
+      }],
+
+      caption1: ['20px', {
+
+        lineHeight: '24px',
+        letterSpacing: '-0.6000000238418579px',
+        fontWeight: '400',
+
+      }],
+
+      caption2: ['18px', {
+
+        lineHeight: '20px',
+        letterSpacing: '-0.30000001192092896px',
+        fontWeight: '400',
+
+      }],
+
+      caption3: ['16px', {
+
+        lineHeight: '18px',
+        letterSpacing: '-0.5px',
+        fontWeight: '400',
+
+      }],
+
+      caption4: ['13px', {
+
+        lineHeight: '15px',
+        letterSpacing: '-0.2000000029802322px',
+        fontWeight: '400',
+
+      }],
+    },
+
     extend: {
       colors: {
         border: "hsl(var(--border))",
