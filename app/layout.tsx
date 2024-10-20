a5195f42d6fa5747899ce3559a9fe4404641ed6d
SanseDev <sansedev@gmail.com>
2024-10-20 03:11:20 +0200

    Home page..

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,8 @@ import "./globals.css";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { MdAccountCircle } from "react-icons/md";
+import { LineChart } from "lucide-react";
+import { Input } from "@/components/ui/input";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -35,43 +37,44 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <main className="min-h-screen flex flex-col items-center">
-            <div className="flex-1 w-full flex flex-col gap-20 items-center">
-              <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-                <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-                  <div className="flex gap-5 items-center font-semibold">
-                    <Image 
-                    alt="Illustration du logo Skall Promo"
-                    width={50}
-                    height={50}
-                    src="/assets/image/skp-logo.png"
-                    className="rounded-2xl"/>
-                  </div>
-                  <Button>
-                    <MdAccountCircle />
-                  </Button>
-                </div>
-              </nav>
-              <div className="flex flex-col gap-20 max-w-5xl p-5">
-                {children}
-              </div>
+          <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+            <div className="w-full max-w-7xl flex justify-between items-center p-3 px-36 text-sm">
 
-              <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-                <p>
-                  Powered by{" "}
-                  <a
-                    href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-                    target="_blank"
-                    className="font-bold hover:underline"
-                    rel="noreferrer"
-                  >
-                    Supabase
-                  </a>
-                </p>
-                <ThemeSwitcher />
-              </footer>
+              <div className="flex gap-5 items-center font-semibold">
+                <Link href="/">
+                <Image
+                  alt="Illustration du logo Skall Promo"
+                  width={50}
+                  height={50}
+                  src="/assets/image/skp-logo.png"
+                  className="rounded-2xl"
+                />
+                </Link>
+              </div>
+              <Link href="/sign-in">
+                <Button variant="ghost" className="rounded-full">
+                  <MdAccountCircle />
+                </Button>
+              </Link>
             </div>
-          </main>
+          </nav>
+          
+          {children}
+
+          <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
+            <p>
+              Powered by{" "}
+              <a
+                href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
+                target="_blank"
+                className="font-bold hover:underline"
+                rel="noreferrer"
+              >
+                Supabase
+              </a>
+            </p>
+            <ThemeSwitcher />
+          </footer>
         </ThemeProvider>
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,49 @@
-import Hero from "@/components/hero";
+import { Box } from "@/components/box/box";
+import { Container } from "@/components/container/container";
 import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
 import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
+import { Button } from "@/components/ui/button";
 import { hasEnvVars } from "@/utils/supabase/check-env-vars";
+import Image from "next/image";
 
 export default async function Index() {
   return (
     <>
-      <Hero />
-      <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-      </main>
+      <Container className="py-12 space-y-10">
+        <div className="w-full border border-gray-200 bg-white rounded shadow-lg px-9 py-9 box bg-gradient-to-r from-indigo-300 to-indigo-500">
+          <Image
+          alt="Illustration du logo Phantom"
+          src="/assets/svg/phantom.svg"
+          width={250}
+          height={250}
+          />
+          <Button>
+            Connect your wallet
+          </Button>
+        </div>
+        <div className="w-full border border-gray-200 bg-white rounded shadow-lg px-9 py-9 box bg-gradient-to-r from-indigo-300 to-indigo-500">
+          <Image
+          alt="Illustration du logo Phantom"
+          src="/assets/svg/phantom.svg"
+          width={250}
+          height={250}
+          />
+          <Button>
+            Connect your wallet
+          </Button>
+        </div>
+        <div className="w-full border border-gray-200 bg-white rounded shadow-lg px-9 py-9 box bg-gradient-to-r from-indigo-300 to-indigo-500">
+          <Image
+          alt="Illustration du logo Phantom"
+          src="/assets/svg/phantom.svg"
+          width={250}
+          height={250}
+          />
+          <Button>
+            Connect your wallet
+          </Button>
+        </div>
+      </Container>
     </>
   );
 }
